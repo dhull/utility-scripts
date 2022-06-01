201f2fcc439c7fb9a51de560f51d67ff7ee6b728
David Hull <github@davidhull.org>
2022-06-01 17:49:58 -0500

    kinesis-dump.py: Add --format option and other improvements.
    * Add `--format` option to output JSON.
    * Write debug messages to stderr.
    * Handle `ProvisionedThroughputExceededException`.
    * Fix non-`--follow` exit condition.  Previously we could exit early if a
      call to `get_records` returned no data.

--- kinesis-dump.py
+++ kinesis-dump.py
@@ -2,6 +2,7 @@
 
 default_profile = 'fbot-sandbox'
 debug = False
+delay_sleep = 0.4 # seconds
 
 import datetime
 import time
@@ -13,16 +14,17 @@ import os
 import pathlib
 import pprint
 import re
+import sys
 
 pp = pprint.PrettyPrinter(indent=4, compact=True)
 
 # Use awscli's virtualenv if we're not already running in one.
 if 'VIRTUAL_ENV' not in os.environ:
-    print('using awscli virtualenv')
+    print('using awscli virtualenv', file=sys.stderr)
     activate_script = pathlib.Path('/usr/local/bin/aws').resolve().parent.joinpath('activate_this.py')
     exec(open(activate_script).read(), dict(__file__=activate_script))
 
-print(os.environ['VIRTUAL_ENV'])
+print(os.environ['VIRTUAL_ENV'], file=sys.stderr)
 
 def parse_since(since):
     """ Parses a "since" value and converts it to seconds."""
@@ -78,14 +80,17 @@ def dump_stream(stream, profile, **opts):
     # more than one shard for any of its Kinesis streams.
 
     shards = client.list_shards(StreamName=stream)
-    if debug: print(f'{bcolors.GREY20}shards\n{pp.pformat(shards)}{bcolors.ENDC}')
+    if debug: print(f'{bcolors.GREY20}shards\n{pp.pformat(shards)}{bcolors.ENDC}', file=sys.stderr)
+    shard_count = len(shards['Shards'])
+    if shard_count > 1:
+        raise Exception(f'shard count {shard_count} > 1 not handled')
     shard_id = shards['Shards'][0]['ShardId']
 
     if opts.get('start', 0):
-        # print(f'using start {opts["start"]}')
+        # print(f'using start {opts["start"]}', file=sys.stderr)
         shard_iterator_opts = { 'ShardIteratorType': 'AT_TIMESTAMP', 'Timestamp': opts['start'] }
     else:
-        # print('using trim_horizon')
+        # print('using trim_horizon', file=sys.stderr)
         shard_iterator_opts = { 'ShardIteratorType': 'TRIM_HORIZON' }
 
     r = client.get_shard_iterator(
@@ -93,27 +98,48 @@ def dump_stream(stream, profile, **opts):
         ShardId=shard_id,
         **shard_iterator_opts)
 
-    if debug: print(f'{bcolors.GREY20}shard iterator\n{pp.pformat(r)}{bcolors.ENDC}');
+    if debug: print(f'{bcolors.GREY20}shard iterator\n{pp.pformat(r)}{bcolors.ENDC}', file=sys.stderr);
 
     itr = r['ShardIterator']
 
     more_data = True
     i = 0
     n = 0
+    format = opts.get('format')
     while more_data:
-        if debug: print(f'\n\n{bcolors.GREY20}fetch {i} itr:{itr}{bcolors.ENDC}')
-        r = client.get_records(ShardIterator=itr)
-        pp.pprint(r)
+        if debug: print(f'\n\n{bcolors.GREY20}fetch {i} itr:{itr}{bcolors.ENDC}', file=sys.stderr)
+        while True:
+            try:
+                r = client.get_records(ShardIterator=itr)
+                if debug: print(pp.pformat(r), file=sys.stderr)
+                break
+            except client.exceptions.ProvisionedThroughputExceededException:
+                print("caught ProvisionedThroughputExceededException", file=sys.stderr)
+                time.sleep(delay_sleep)
+                pass
+            except Exception as e:
+                print(e, file=sys.stderr)
+                raise e
+
         for d in r['Records']:
             n += 1
             if stream in handler:
                 d = handler[stream](d)
-            print(f'\n{bcolors.GREY10}record {n}:{bcolors.ENDC}')
-            pp.pprint(json.loads(d['Data']))
+            print(f'\n{bcolors.GREY10}record {n}:{bcolors.ENDC}', file=sys.stderr)
+            if format == 'python':
+                pp.pprint(json.loads(d['Data']))
+            elif format == 'json':
+                sys.stdout.buffer.write(d['Data'])
+                sys.stdout.buffer.write(b'\n')
+                sys.stdout.buffer.flush()
+            else:
+                raise Exception(f'unknown format ${format}')
         itr = r['NextShardIterator']
         if args.limit and n >= args.limit:
             more_data = False
-        elif len(r['Records']) == 0:
+        elif itr == None:
+            more_data = False
+        elif r['MillisBehindLatest'] == 0:
             if args.follow:
                 time.sleep(5)
             else:
@@ -123,7 +149,7 @@ def dump_stream(stream, profile, **opts):
             # rate-limit is applied across all consumers, and clients will
             # receive ProvisionedThroughputExceededException if it is
             # exceeded.
-            time.sleep(0.5)
+            time.sleep(delay_sleep)
         i += 1
 
 ########################################################################
@@ -131,10 +157,13 @@ def dump_stream(stream, profile, **opts):
 parser = argparse.ArgumentParser(description='Dump a Kinesis stream.')
 parser.add_argument('stream')
 parser.add_argument(
+    '-f', '--format', choices=['json', 'python'], default='python',
+    help=f'output format (default python)')
+parser.add_argument(
     '-p', '--profile', default=default_profile,
     help=f'specify AWS profile (default {default_profile})')
 parser.add_argument(
-    '-f', '--follow', action='store_true',
+    '--follow', action='store_true',
     help='continue waiting for more data when the end of the stream is reached')
 parser.add_argument(
     '-n', '--limit', type=int,
@@ -158,4 +187,4 @@ if args.start:
 elif args.since:
     start = datetime.datetime.fromtimestamp(time.time() - args.since)
 
-dump_stream(args.stream, profile=args.profile, follow=args.follow, limit=args.limit, start=start)
+dump_stream(args.stream, profile=args.profile, follow=args.follow, limit=args.limit, start=start, format=args.format)
